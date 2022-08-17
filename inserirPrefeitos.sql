05f19832c0bc01ffe0d8296f8c3e8851e3fcb2a0
Victor Gabriel <victo091198@gmail.com>
2022-08-17 19:43:57 -0300

    Inserted some prefeitos

---
+++ inserirPrefeitos.sql
@@ -0,0 +1,15 @@
+select * from cidades;
+
+INSERT INTO prefeitos 
+    (nome, cidade_id)
+VALUES 
+    ('Rodrigo Neves', 2),
+    ('Raquel lyra', 3),
+    ('Zenaldo Coutinho', Null);
+
+select * from prefeitos;
+
+INSERT INTO prefeitos 
+    (nome, cidade_id)
+VALUES 
+    ('Rafael Greca', null);
