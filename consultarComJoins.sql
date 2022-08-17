28de13c84a1cdbb959b631ea38c519c8cdb4acfd
Victor Gabriel <victo091198@gmail.com>
2022-08-17 19:55:14 -0300

    Learning joins

---
+++ consultarComJoins.sql
@@ -0,0 +1,3 @@
+select * from cidades as c inner join prefeitos as p on c.id = p.cidade_id;
+select * from cidades c left join prefeitos p on c.id = p.cidade_id;
+select * from cidades c right join prefeitos p on c.id = p.cidade_id;
